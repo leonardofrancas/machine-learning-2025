ea89b800a41e504beb9c29c9fe407ebc2c7e9a96
leo <leofrancas@hotmail.com>
2025-10-24 09:26:35 -0300

    Tree Frutas

--- semana_01/frutas.py
+++ semana_01/frutas.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-df = pd.read_excel("data/dados_frutas.xlsx")
+df = pd.read_excel("../data/dados_frutas.xlsx")
 df
 
 # %%
@@ -35,4 +35,48 @@ tree.plot_tree(arvore,
 #%%
 
 proba = arvore.predict_proba([[1,1,1,1]])[0]
+pd.Series(proba, index=arvore.classes_)
+# %%
+# Inicio Machine Learning
+
+import pandas as pd
+
+df = pd.read_excel("../data/dados_frutas.xlsx")
+df
+
+#%%
+
+from sklearn import tree
+
+arvore = tree.DecisionTreeClassifier()
+
+# %%
+
+y = df['Fruta']
+caracteristicas = ["Arredondada","Suculenta",'Vermelha','Doce']
+X = df[caracteristicas]
+
+
+# %%
+
+arvore.fit(X, y)
+
+# %%
+
+arvore.predict([[0, 0, 0, 0]])
+
+# %%
+
+import matplotlib.pyplot as plt
+
+plt.figure(dpi=400, figsize=[4,4])
+
+tree.plot_tree(arvore,
+               feature_names=caracteristicas,
+               class_names=y.unique(),
+               filled=True)
+
+# %%
+
+proba = arvore.predict_proba([[1, 1, 1, 1]])[0]
 pd.Series(proba, index=arvore.classes_)
