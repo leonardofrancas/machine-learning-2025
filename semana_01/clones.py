39cc5e5ceb5fd847f6902719c479c28a7ba8675f
leo <leofrancas@hotmail.com>
2025-11-05 09:06:35 -0300

    Star Wars

--- semana_01/clones.py
+++ semana_01/clones.py
@@ -2,7 +2,21 @@
 
 import pandas as pd
 
-df = pd.read_parquet("data/dados_clones.parquet")
+df = pd.read_parquet("../data/dados_clones.parquet")
+
+df['General Jedi encarregado'].unique()
+
+# %%
+
+features = ['Massa(em kilos)', 'Estatura(cm)']
+
+df.groupby('Status ')[features].mean()
+
+# %%
+
+import pandas as pd
+
+df = pd.read_parquet("../data/dados_clones.parquet")
 
 df['General Jedi encarregado'].unique()
 
