8a2672c6ac2f4e2bcf0099761f7f71e6a11b0abf
leo <leofrancas@hotmail.com>
2025-11-05 09:11:31 -0300

    Star Wars

--- semana_01/clones.py
+++ semana_01/clones.py
@@ -22,6 +22,13 @@ df['General Jedi encarregado'].unique()
 
 # %%
 
+from sklearn import tree
+
+tree.plot_tree(max_depth=3)
+
+
+# %%
+
 features = ['Massa(em kilos)', 'Estatura(cm)']
 
 df.groupby('Status ')[features].mean()
